d2545b395fbb5b280e630a7a9bcea6c7a18c1e58
wuym <275058342@qq.com>
2015-06-19 11:08:22 +0800

    333333

--- src/com/fl/media/JpgToAvi.java
+++ src/com/fl/media/JpgToAvi.java
@@ -33,6 +33,7 @@ public class JpgToAvi {
 		System.out.println("2");
 		System.out.println("2");
 		System.out.println("2");
+		System.out.println("2");
 		System.out.println("3");
 		System.out.println("4");
 		System.out.println("5");
